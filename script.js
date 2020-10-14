dfa1708133b1ae471c7415c48263eb5367dc3367
Jake Fireside <labountypaul@icloud.com>
2020-10-14 16:08:28 -0500

    updated row and columns and added buttons and textarea

---
+++ script.js
@@ -0,0 +1,32 @@
+$(".am9-btn").on("click", function(){
+    var amText = $("#9am").val()
+    console.log(amText)
+})
+$(".am10-btn").on("click", function(){
+    var amText = $("#10am").val()
+    console.log(amText)
+})
+$(".am11-btn").on("click", function(){
+    var amText = $("#11am").val()
+    console.log(amText)
+})
+$(".pm12-btn").on("click", function(){
+    var amText = $("#12pm").val()
+    console.log(amText)
+})
+$(".pm1-btn").on("click", function(){
+    var amText = $("#1pm").val()
+    console.log(amText)
+})
+$(".pm2-btn").on("click", function(){
+    var amText = $("#2pm").val()
+    console.log(amText)
+})
+$(".pm3-btn").on("click", function(){
+    var amText = $("#3pm").val()
+    console.log(amText)
+})
+$(".pm4-btn").on("click", function(){
+    var amText = $("#4pm").val()
+    console.log(amText)
+})
